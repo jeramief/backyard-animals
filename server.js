0b9c2052af8b55f06bc0bfa1564ca301b7bd6b2f
jeramief <109633173+jeramief@users.noreply.github.com>
2023-05-04 16:20:38 -0400

    commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const animals = {
+	'cardinal': {
+		'species': 'bird',
+		'color': 'red',
+	},
+	'gray squirrel': {
+		'species': 'rodent',
+		'color': 'gray',
+	},
+	'porcupine': {
+		'species': 'rodent',
+		'color': 'brown',
+	},
+	'unknown': {
+		'species': 'unknown',
+		'color': 'unknown',
+	}
+}
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api', (req, res) => {
+	res.json(animals)
+})
+
+app.get('/api/:species', (req, res) => {
+	// console.log(req.params.species)
+	const species = req.params.species.toLowerCase()
+	if(animals[species]) {
+		res.json(animals[species])
+	} else {
+		res.json(animals['unknown'])
+	}
+})
+
+app.listen(PORT, () => {
+	console.log(`Port: ${PORT}`)
+})
